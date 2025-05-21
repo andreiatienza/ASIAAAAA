6ec8e5e899c413528701d1834511498f2045c10e
andreiatienza <bonlasatienza@gmail.com>
2025-05-21 08:09:11 +0800

    Update DB config to use remote MySQL

--- sdg-dashboard/backend/config/db.js
+++ sdg-dashboard/backend/config/db.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST || 'dpg-d0mgs2u3jp1c738bg0o0-a',      // your database host
+  host: process.env.DB_HOST || 'dpg-d0mgs2u3jp1c738bg0o0-a.pg.render.com',
   user: process.env.DB_USER || 'andrei',      // your database username
   password: process.env.DB_PASS || 'kksWqZQfxBUA0rUHLXQBjWibUT2kq382',  // your database password
   database: process.env.DB_NAME || 'asia_db_mubo',  // fallback to 'asia_db' if env var missing
